529b43debb09ec8eabf90c20818f70a16605a694
bolotsmaria <bolotsmaria@gmail.com>
2022-12-03 05:10:09 +0200

    Original copy of bolotska

---
+++ lab08/dist/html/search/all_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['true_2',['true',['../main_8c.html#a842f4dc2ca851d7c1967e956601c0662',1,'main.c']]]
+];
---
+++ lab08/dist/html/search/searchdata.js
@@ -0,0 +1,24 @@
+var indexSectionsWithContent =
+{
+  0: "mt",
+  1: "m",
+  2: "m",
+  3: "t"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "files",
+  2: "functions",
+  3: "variables"
+};
+
+var indexSectionLabels =
+{
+  0: "Всі",
+  1: "Файли",
+  2: "Функції",
+  3: "Змінні"
+};
+
